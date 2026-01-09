b85961c9e41e76ff29f7510db1907f4af33e0abf
tsquared96 <n2vsb@nycap.rr.com>
2026-01-09 16:46:59 -0500

    Refactor port definitions for ESP8266
    Updated port definitions to avoid redefinition and added conditional compilation.

--- components/bestway_spa/ports.h
+++ components/bestway_spa/ports.h
@@ -1,23 +1,21 @@
+#ifndef PORTS_H
+#define PORTS_H
+
+#include <Arduino.h>
+
 /**
- * Port definitions for ESP8266 direct GPIO register access
- *
- * From VisualApproach WiFi-remote-for-Bestway-Lay-Z-SPA
- * https://github.com/visualapproach/WiFi-remote-for-Bestway-Lay-Z-SPA
- *
- * Licensed under GPL v3
+ * ESP8266 High-Speed GPIO Register Definitions
+ * These allow the driver to read/write pins in nanoseconds, 
+ * which is required to keep up with the Spa's clock signal.
  */
 
-#pragma once
-
-#ifdef ESP8266
-// Direct port manipulation memory addresses for ESP8266
-#define PIN_OUT         0x60000300
-#define PIN_OUT_SET     0x60000304
-#define PIN_OUT_CLEAR   0x60000308
-
-#define PIN_DIR         0x6000030C
-#define PIN_DIR_OUTPUT  0x60000310
-#define PIN_DIR_INPUT   0x60000314
-
-#define PIN_IN          0x60000318
+#ifndef PIN_OUT
+  #define PIN_OUT         0x60000300
+  #define PIN_OUT_SET     0x60000304
+  #define PIN_OUT_CLEAR   0x60000308
+  #define PIN_DIR_OUTPUT  0x6000030C
+  #define PIN_DIR_INPUT   0x60000310
+  #define PIN_IN          0x60000318
 #endif
+
+#endif // PORTS_H
