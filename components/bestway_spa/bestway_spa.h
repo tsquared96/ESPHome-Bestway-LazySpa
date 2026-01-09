fcba2c0a5a9b657aae2bb82bdddf979752b327dc
tsquared96 <n2vsb@nycap.rr.com>
2026-01-09 17:55:51 -0500

    Update bestway_spa.h

--- components/bestway_spa/bestway_spa.h
+++ components/bestway_spa/bestway_spa.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/climate/climate.h"
 #include "CIO_TYPE1.h"
+#include "DSP_TYPE1.h"
 #include "enums.h"
 
 namespace esphome {
@@ -14,25 +15,40 @@ class BestwaySpa : public climate::Climate, public Component {
   void loop() override;
   void dump_config() override;
 
+  // Configuration setters for Python/YAML
   void set_protocol_type(ProtocolType type) { protocol_type_ = type; }
+  
+  // CIO Pin Setters
   void set_cio_data_pin(InternalGPIOPin *pin) { cio_data_pin_ = pin; }
   void set_cio_clk_pin(InternalGPIOPin *pin) { cio_clk_pin_ = pin; }
   void set_cio_cs_pin(InternalGPIOPin *pin) { cio_cs_pin_ = pin; }
 
-  // Button interaction for Lambdas
-  void on_button_press_(ButtonCode code);
+  // DSP Pin Setters (Missing until now)
+  void set_dsp_data_pin(InternalGPIOPin *pin) { dsp_data_pin_ = pin; }
+  void set_dsp_clk_pin(InternalGPIOPin *pin) { dsp_clk_pin_ = pin; }
+  void set_dsp_cs_pin(InternalGPIOPin *pin) { dsp_cs_pin_ = pin; }
 
   // Climate overrides
   climate::ClimateTraits traits() override;
   void control(const climate::ClimateCall &call) override;
 
+  // Helper for text sensors/lambdas
+  std::string get_display_text_sensor() { return va_cio_type1.get_display(); }
+
  protected:
   ProtocolType protocol_type_;
+  
   InternalGPIOPin *cio_data_pin_;
   InternalGPIOPin *cio_clk_pin_;
   InternalGPIOPin *cio_cs_pin_;
 
+  // Optional DSP pins
+  InternalGPIOPin *dsp_data_pin_{nullptr};
+  InternalGPIOPin *dsp_clk_pin_{nullptr};
+  InternalGPIOPin *dsp_cs_pin_{nullptr};
+
   CIO_TYPE1 va_cio_type1;
+  DSP_TYPE1 va_dsp_type1; // Added to match the DSP headers
 
   void update_sensors();
 };
