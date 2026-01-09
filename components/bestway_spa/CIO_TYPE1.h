62ae9149d5b22cac31070f0ad081fecbd8f8cefe
tsquared96 <n2vsb@nycap.rr.com>
2026-01-09 15:23:24 -0500

    Update CIO_TYPE1.h to modify brightness and add packet error
    Made the brightness variable public and added a packet error variable.

--- components/bestway_spa/CIO_TYPE1.h
+++ components/bestway_spa/CIO_TYPE1.h
@@ -15,15 +15,13 @@ public:
     virtual bool getHasjets() = 0;
     virtual bool getHasair() = 0;
 
-    // ISRs must be in IRAM for speed
     void IRAM_ATTR isr_packetHandler();
     void IRAM_ATTR isr_clkHandler();
 
     sStates cio_states;
+    uint8_t brightness = 7; // Fixed: Now public
     uint32_t good_packets_count = 0;
     uint32_t bad_packets_count = 0;
-    
-    // Debug counters
     volatile uint32_t cmd_read_count = 0;
     volatile uint32_t button_bits_sent = 0;
 
@@ -42,6 +40,7 @@ protected:
     volatile uint8_t _received_byte;
     volatile uint8_t _brightness;
     volatile uint8_t _payload[11];
+    volatile uint8_t _packet_error;
     volatile bool _data_is_output;
     volatile bool _new_packet_available;
     volatile bool _packet_transm_active;
@@ -52,11 +51,16 @@ protected:
     static const uint8_t DSP_CMD2_DATAREAD = 0x42;
     static const uint8_t DSP_CMD2_DATAWRITE = 0x40;
 
+#ifdef ESP8266
     static const uint32_t PIN_IN = 0x60000318;
     static const uint32_t PIN_OUT_SET = 0x60000304;
     static const uint32_t PIN_OUT_CLEAR = 0x60000308;
     static const uint32_t PIN_DIR_INPUT = 0x60000314;
     static const uint32_t PIN_DIR_OUTPUT = 0x60000310;
+#endif
+
+    static const uint8_t CHARCODES[38];
+    static const char CHARS[38];
 };
 
 class CIO_PRE2021 : public CIO_TYPE1 {
@@ -69,5 +73,4 @@ private:
 };
 
 extern CIO_TYPE1* g_cio_instance;
-
 }
