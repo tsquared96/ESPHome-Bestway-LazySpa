003ee95c139c22e8cfcdc0ddd391282651201154
tsquared96 <n2vsb@nycap.rr.com>
2026-01-09 16:57:05 -0500

    Remove comments for setter functions
    Removed comments for setter functions to improve code clarity.

--- components/bestway_spa/bestway_spa.h
+++ components/bestway_spa/bestway_spa.h
@@ -49,7 +49,6 @@ class BestwaySpa : public climate::Climate, public Component {
   climate::ClimateTraits traits() override;
   void control(const climate::ClimateCall &call) override;
 
-  // Setters for Pins
   void set_protocol_type(ProtocolType type) { protocol_type_ = type; }
   void set_cio_data_pin(InternalGPIOPin *pin) { cio_data_pin_ = pin; }
   void set_cio_clk_pin(InternalGPIOPin *pin) { cio_clk_pin_ = pin; }
@@ -59,7 +58,6 @@ class BestwaySpa : public climate::Climate, public Component {
   void set_dsp_cs_pin(InternalGPIOPin *pin) { dsp_cs_pin_ = pin; }
   void set_audio_pin(InternalGPIOPin *pin) { audio_pin_ = pin; }
 
-  // Setters for Sensors
   void set_current_temperature_sensor(sensor::Sensor *s) { current_temp_sensor_ = s; }
   void set_target_temperature_sensor(sensor::Sensor *s) { target_temp_sensor_ = s; }
   void set_power_sensor(binary_sensor::BinarySensor *s) { power_sensor_ = s; }
