75cc2ebb4e76240c97ccdf45e89221b92384078f
tsquared96 <n2vsb@nycap.rr.com>
2026-01-09 16:18:26 -0500

    Update from Gemini

--- components/bestway_spa/CIO_4W.cpp
+++
@@ -1,6 +0,0 @@
-#include "CIO_TYPE1.h" // Or whichever base class it uses
-
-namespace bestway_va {
-// Just a placeholder to prevent "No such file" errors
-// without actually containing broken logic.
-}
--- components/bestway_spa/CIO_TYPE2.cpp
+++
@@ -1,5 +0,0 @@
-#include "CIO_TYPE1.h"
-
-namespace bestway_va {
-// Placeholder for 2021+ Type 2 Spas
-}
