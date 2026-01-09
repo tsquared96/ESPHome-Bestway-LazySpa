0c10b186d430e10b44953cfdc66c10517eae7551
tsquared96 <n2vsb@nycap.rr.com>
2026-01-09 17:03:45 -0500

    Update __init__.py

--- components/bestway_spa/__init__.py
+++ components/bestway_spa/__init__.py
@@ -2,7 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-# This links to the "namespace esphome { namespace bestway_spa {" in your C++
 bestway_spa_ns = cg.esphome_ns.namespace("bestway_spa")
 BestwaySpa = bestway_spa_ns.class_("BestwaySpa", cg.Component)
 
