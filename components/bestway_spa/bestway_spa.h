4d81a558e503c89a8fec6e3312e7c915c43c6bfa
tsquared96 <n2vsb@nycap.rr.com>
2026-01-09 18:06:52 -0500

    Update bestway_spa.h

--- components/bestway_spa/bestway_spa.h
+++ components/bestway_spa/bestway_spa.h
@@ -2,6 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/climate/climate.h"
+#include "esphome/components/sensor/sensor.h"
 #include "CIO_TYPE1.h"
 #include "DSP_TYPE1.h"
 #include "enums.h"
@@ -11,11 +12,12 @@ namespace bestway_spa {
 
 class BestwaySpa : public climate::Climate, public Component {
  public:
+  BestwaySpa() {}
   void setup() override;
   void loop() override;
   void dump_config() override;
 
-  // Setters for Python/YAML
+  // Configuration setters for Python
   void set_protocol_type(ProtocolType type) { protocol_type_ = type; }
   void set_cio_data_pin(InternalGPIOPin *pin) { cio_data_pin_ = pin; }
   void set_cio_clk_pin(InternalGPIOPin *pin) { cio_clk_pin_ = pin; }
@@ -25,14 +27,16 @@ class BestwaySpa : public climate::Climate, public Component {
   void set_dsp_clk_pin(InternalGPIOPin *pin) { dsp_clk_pin_ = pin; }
   void set_dsp_cs_pin(InternalGPIOPin *pin) { dsp_cs_pin_ = pin; }
 
-  // FIX: Missing Method Declaration from Claude's review
+  // Sensor setters used by sensor.py
+  void set_current_temperature_sensor(sensor::Sensor *sens) { current_temp_sensor_ = sens; }
+  void set_target_temperature_sensor(sensor::Sensor *sens) { target_temp_sensor_ = sens; }
+
+  // Logic bridge
   void on_button_press_(ButtonCode code);
 
-  // Climate overrides
+  // Climate implementation
   climate::ClimateTraits traits() override;
   void control(const climate::ClimateCall &call) override;
-
-  std::string get_display_text_sensor() { return va_cio_type1.get_display(); }
 
  protected:
   ProtocolType protocol_type_;
@@ -43,6 +47,9 @@ class BestwaySpa : public climate::Climate, public Component {
   InternalGPIOPin *dsp_data_pin_{nullptr};
   InternalGPIOPin *dsp_clk_pin_{nullptr};
   InternalGPIOPin *dsp_cs_pin_{nullptr};
+
+  sensor::Sensor *current_temp_sensor_{nullptr};
+  sensor::Sensor *target_temp_sensor_{nullptr};
 
   CIO_TYPE1 va_cio_type1;
   DSP_TYPE1 va_dsp_type1;
