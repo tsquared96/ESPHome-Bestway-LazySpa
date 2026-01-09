9aacf68ec3e9d8b5a000166ebdb217b65904474e
tsquared96 <n2vsb@nycap.rr.com>
2026-01-09 16:52:43 -0500

    Refactor DSP_TYPE1 class and update pin types

--- components/bestway_spa/DSP_TYPE1.h
+++ components/bestway_spa/DSP_TYPE1.h
@@ -1,47 +1,40 @@
-#pragma once
-#include <Arduino.h>
-#include "enums.h"
+#ifndef DSP_TYPE1_H
+#define DSP_TYPE1_H
 
-// ESP8266 Register access for high-speed bit-banging
-#ifndef PIN_IN
-#define PIN_IN          0x60000318
-#define PIN_OUT_SET     0x60000304
-#define PIN_OUT_CLEAR   0x60000308
-#define PIN_DIR_OUTPUT  0x60000300
-#define PIN_DIR_INPUT   0x60000300
-#endif
+#include <Arduino.h>
+#include "ports.h"
+#include "bestway_va_types.h"
 
 namespace bestway_va {
 
+// The DSP driver handles communication to the physical Display panel
 class DSP_TYPE1 {
 public:
-    DSP_TYPE1();
-    void setup(int dsp_data_pin, int dsp_clk_pin, int dsp_cs_pin, int audio_pin = -1);
+    void setup(uint8_t data_pin, uint8_t clk_pin, uint8_t cs_pin, int audio_pin);
     void handleStates();
-    Buttons getPressedButton();
-
-    // Interrupt handlers must be IRAM_ATTR and public for the glue code
-    void IRAM_ATTR isr_packetHandler();
+    
+    // Interrupt Handlers
     void IRAM_ATTR isr_clkHandler();
+    void IRAM_ATTR isr_packetHandler();
 
-    // This is shared with the CIO driver to sync the display state
-    sStates dsp_states;
+    struct {
+        char display_text[5]; // e.g., " 38", "FIL", "END"
+        bool power_led;
+        bool heater_led;
+    } dsp_states;
 
-protected:
-    int _DATA_PIN;
-    int _CLK_PIN;
-    int _CS_PIN;
-    int _AUDIO_PIN;
+private:
+    uint8_t _data_pin, _clk_pin, _cs_pin;
+    int _audio_pin;
+    uint32_t _data_mask, _clk_mask, _cs_mask;
 
-    // Volatile variables modified inside the ISRs
-    volatile uint8_t _byte_count;
-    volatile uint8_t _bit_count;
-    volatile uint8_t _received_byte;
-    volatile uint8_t _payload[11];
-    volatile bool _packet_transm_active;
-    volatile bool _new_packet_available;
-
-    Buttons _last_btn{NOBTN};
+    volatile uint16_t _captured_words[10];
+    volatile uint8_t _bit_idx = 0;
+    volatile uint8_t _word_idx = 0;
+    
+    char _getChar(uint16_t code);
 };
 
 } // namespace bestway_va
+
+#endif
