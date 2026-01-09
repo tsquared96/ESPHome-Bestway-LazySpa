e0616c8fb6fd30a476be246b676621cc798660df
tsquared96 <n2vsb@nycap.rr.com>
2026-01-09 16:42:01 -0500

    Enhance BestwaySpa class with static instance and interrupts
    Added static instance and interrupt handling methods for ESP8266. Updated configuration setters and removed unused methods.

--- components/bestway_spa/bestway_spa.h
+++ components/bestway_spa/bestway_spa.h
@@ -8,14 +8,12 @@
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 
-// Include the VisualApproach Drivers
 #include "CIO_TYPE1.h"
 #include "DSP_TYPE1.h"
 
 namespace esphome {
 namespace bestway_spa {
 
-// Bridge the namespaces so the compiler knows these types
 using Buttons = bestway_va::Buttons;
 
 enum ProtocolType {
@@ -37,6 +35,13 @@ struct SpaState {
 
 class BestwaySpa : public climate::Climate, public Component {
  public:
+  // Static instance for interrupt handling on ESP8266
+  static BestwaySpa *instance;
+  static void IRAM_ATTR cio_clk_intr();
+  static void IRAM_ATTR cio_cs_intr();
+  static void IRAM_ATTR dsp_clk_intr();
+  static void IRAM_ATTR dsp_cs_intr();
+
   void setup() override;
   void loop() override;
   void dump_config() override;
@@ -45,7 +50,7 @@ class BestwaySpa : public climate::Climate, public Component {
   climate::ClimateTraits traits() override;
   void control(const climate::ClimateCall &call) override;
 
-  // Configuration setters for Pins
+  // Setters
   void set_protocol_type(ProtocolType type) { protocol_type_ = type; }
   void set_cio_data_pin(InternalGPIOPin *pin) { cio_data_pin_ = pin; }
   void set_cio_clk_pin(InternalGPIOPin *pin) { cio_clk_pin_ = pin; }
@@ -55,7 +60,6 @@ class BestwaySpa : public climate::Climate, public Component {
   void set_dsp_cs_pin(InternalGPIOPin *pin) { dsp_cs_pin_ = pin; }
   void set_audio_pin(InternalGPIOPin *pin) { audio_pin_ = pin; }
 
-  // Configuration setters for Sensors (Required by climate.py)
   void set_current_temperature_sensor(sensor::Sensor *s) { current_temp_sensor_ = s; }
   void set_target_temperature_sensor(sensor::Sensor *s) { target_temp_sensor_ = s; }
   void set_power_sensor(binary_sensor::BinarySensor *s) { power_sensor_ = s; }
@@ -64,19 +68,12 @@ class BestwaySpa : public climate::Climate, public Component {
   void set_bubbles_sensor(binary_sensor::BinarySensor *s) { bubbles_sensor_ = s; }
   void set_display_text_sensor(text_sensor::TextSensor *s) { display_text_sensor_ = s; }
 
-  // Public method for the lambda buttons in your YAML
   void on_button_press_(Buttons btn);
 
  protected:
-  void handle_6wire_protocol_();
-  void handle_4wire_protocol_() {} 
-  void process_button_queue_();
-
-  // VA Drivers
   bestway_va::CIO_PRE2021 va_cio_type1;
   bestway_va::DSP_TYPE1 va_dsp_type1; 
 
-  // Pin Pointers
   ProtocolType protocol_type_{PROTOCOL_6WIRE_T1};
   InternalGPIOPin *cio_clk_pin_{nullptr};
   InternalGPIOPin *cio_data_pin_{nullptr};
@@ -86,7 +83,6 @@ class BestwaySpa : public climate::Climate, public Component {
   InternalGPIOPin *dsp_cs_pin_{nullptr};
   InternalGPIOPin *audio_pin_{nullptr};
 
-  // Sensor Pointers
   sensor::Sensor *current_temp_sensor_{nullptr};
   sensor::Sensor *target_temp_sensor_{nullptr};
   binary_sensor::BinarySensor *power_sensor_{nullptr};
@@ -95,11 +91,9 @@ class BestwaySpa : public climate::Climate, public Component {
   binary_sensor::BinarySensor *bubbles_sensor_{nullptr};
   text_sensor::TextSensor *display_text_sensor_{nullptr};
 
-  // Internal State
   SpaState state_;
   bool dsp_enabled_{false};
   std::queue<Buttons> button_queue_;
-  uint16_t current_button_code_{0x1B1B};
 };
 
 } // namespace bestway_spa
